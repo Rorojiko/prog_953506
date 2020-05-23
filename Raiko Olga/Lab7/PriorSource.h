5de5b07cb6dac7bb11a0b97481ede0f3dbb6d120
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2020-05-23 20:26:40 +0300

    Lab 1-7

---
+++ Raiko Olga/Lab7/PriorSource.h
@@ -0,0 +1,4 @@
+unsigned long long int hashing(char* key);
+void show_history(unsigned long long int account);
+double get_percent(int sum, const char* from, const char* to);
+char* to_string_txt(unsigned long long int account);
