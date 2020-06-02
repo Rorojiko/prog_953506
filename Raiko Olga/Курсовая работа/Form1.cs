12980574c43d52a13cb404a76c952b64b4566062
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2020-06-02 18:00:54 +0300

    Курсовая

---
+++ Raiko Olga/Курсовая работа/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Попытканомер2
+{
+    public partial class Form1 : Form
+    { int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = new WebBrowser();
+            web.Visible = true;
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.DocumentCompleted += Web_DocumentCompleted;
+            tabControl1.TabPages.Add("New Page");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i++;
+        }
+
+        private void Web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle + "TheBrauzer";
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoBack();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoForward();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Refresh();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (toolStripTextBox1.Text != null)
+            {
+                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripTextBox1.Text);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count > 1)
+            {
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                tabControl1.SelectTab(tabControl1.TabPages.Count - 1);
+                i -= 1;
+            }
+            else { Application.Exit(); }
+        }
+
+        private void toolStripTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripTextBox1.Text);
+            }
+        }
+       /* private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (toolStripComboBox1.SelectedIndex)
+            {
+                case 0: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("www.vk.com"); break;
+                case 1: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("olocoder.ru"); break;
+                case 2: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("www.youtube.com"); break;
+                case 3: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("www.google.com"); break;
+                case 4: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("www.yandex.ru"); break;
+                case 5: ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("www.mail.ru"); break;
+            }
+        }*/
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoHome();
+        }
+        
+    }
+}
